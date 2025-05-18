e907c0fa11e5bb5f050e90b312bc1200aae34097
Brendon Dugan <brendondugan@gmail.com>
2025-05-18 17:22:13 +0100

    Removed the player from the tree during the main menu
    And filled out the test level for the demo game a bit

--- engine/abstract_game_manager.gd
+++ engine/abstract_game_manager.gd
@@ -3,12 +3,14 @@ extends Node2D
 
 @export var level_holder: Node
 @export var player: AbstractPlayer
+@export var main_menu: AbstractMenu
 
 var level_map: Dictionary[String, AbstractLevel] = {} 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameStateEvents.SCENE_CHANGE_REQUESTED.connect(handle_scene_change_requested)
+	player.get_parent().remove_child(player)
 
 func is_valid_level(levelName: String) -> bool:
 	if level_map.has(levelName):
@@ -32,6 +34,8 @@ func handle_scene_change_requested(requestedScene: String) -> void:
 			print("There is no player, we cannot proceed")
 			return
 		level.player = player
+		if not player.is_inside_tree():
+			self.add_child(player)
 		if not level_holder:
 			print("The Level is missing a LevelHolder (of type StaticBody2D), we cannot proceed")
 			return
