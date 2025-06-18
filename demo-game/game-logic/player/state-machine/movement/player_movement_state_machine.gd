afcba3933ddbc75192568ef5f9416f056638128f
Brendon Dugan <brendondugan@gmail.com>
2025-06-18 18:14:48 +0100

    Cleaned up icons
    Removed some unneeded code

--- demo-game/game-logic/player/state-machine/movement/player_movement_state_machine.gd
+++ demo-game/game-logic/player/state-machine/movement/player_movement_state_machine.gd
@@ -1,11 +1,9 @@
 class_name PlayerMovementStateMachine
 extends Node
 
-@export
-var starting_state: PlayerMovementState
+
+@export var starting_state: PlayerMovementState
 @export var pause_state: PlayerMovementState
-#@export
-#var combat_state: PlayerMovementState
 var current_state: PlayerMovementState
 
 func init(parent: Player) -> void:
@@ -13,14 +11,9 @@ func init(parent: Player) -> void:
 		child.parent = parent
 	change_state(starting_state)
 	GameStateEvents.MENU_OPEN.connect(_handle_menu_opened)
-	#MessageBus.COMBAT_INITIATED.connect(handle_combat_state)
 
 func _handle_menu_opened() -> void:
-	print("Menu Opened")
 	change_state(pause_state)
-
-#func handle_combat_state() -> void:
-	#change_state(combat_state)
 
 func change_state(new_state: PlayerMovementState) -> void:
 	if current_state:
