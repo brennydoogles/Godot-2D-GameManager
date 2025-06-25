4455bf2d83a8d30853afb145ea6c1c3c7e068539
Brendon Dugan <brendondugan@gmail.com>
2025-06-25 21:54:05 +0100

    Update level_transition_trigger.gd

--- engine/level/level_transition_trigger.gd
+++ engine/level/level_transition_trigger.gd
@@ -2,7 +2,8 @@ class_name LevelTransitionTrigger
 extends Area2D
 
 @export var targetScene: String
-@export var player_start_override: Vector2
+@export var player_start_override: Vector2 = Vector2.INF
+@export var transition_name: String = "fade_to_black"
 
 
 func _ready() -> void:
@@ -11,6 +12,6 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is AbstractPlayer:
 		if player_start_override and player_start_override != Vector2.INF:
-			GameStateEvents.LEVEL_CHANGE_WITH_START_POSITION_OVERRIDE_REQUESTED.emit(targetScene, "fade_to_black", player_start_override)
+			GameStateEvents.LEVEL_CHANGE_WITH_START_POSITION_OVERRIDE_REQUESTED.emit(targetScene, transition_name, player_start_override)
 		else:
-			GameStateEvents.LEVEL_CHANGE_REQUESTED.emit(targetScene, "fade_to_black")
+			GameStateEvents.LEVEL_CHANGE_REQUESTED.emit(targetScene, transition_name)
